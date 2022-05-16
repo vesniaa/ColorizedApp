d40404ee06ff5ac2e33a2c902085a037be8cc22e
Eugenia <pyssiki@yandex.ru>
2022-05-16 14:50:47 +0500

    Initial Commit

---
+++ ColorizedApp/ColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppApp.swift
+//  ColorizedApp
+//
+//  Created by Евгения Аникина on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
